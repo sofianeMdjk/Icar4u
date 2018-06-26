9d9ffe3ad3ad71fddcf37dec44fb2887e7455965
ressay <bourahla.yasser@gmail.com>
2018-06-26 19:24:08 +0100

    fixing errors

--- accident_catcher.py
+++ accident_catcher.py
@@ -1,17 +1,34 @@
 import numpy
 
-from video_preprocessing import prepare_video, frames_count
+from video_preprocessing import prepare_video, frames_count, load_set
 from keras.models import load_model
 
 
+
+def prepare_video(video_file,rand,model):
+
+
+
+    sequence = numpy.zeros((1,99,144,256))
+
+    for i in range(rand):
+        t, errors = load_set(video_file, i)
+        if numpy.shape(t) == (99, 144, 256):
+            sequence[0] = t
+            prediction = model.predict(sequence)
+            print("prediction: " + str(i))
+            print(prediction)
+
+    return sequence
+
+
 model = load_model('model.hdf5')
+video_path = 'test.mp4'
+num_frames = frames_count(video_path)
+rand = int(frames_count(video_path) - 100)
 
-video_path = 'video2.mp4'
-
-rand = (int) (frames_count(video_path) - 99)
-
-data = prepare_video(video_path,min(rand,5))
-print(str(numpy.shape(data)))
-# print(str(data))
-prediction = model.predict(data)
-print(prediction)
+data = prepare_video(video_path,min(rand,9000),model)
+# print(str(numpy.shape(data)))
+# # print(str(data))
+# prediction = model.predict(data)
+# print(prediction)
